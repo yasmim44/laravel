b771bc7a1539ed313a02999fed5d5fcbbbc84e92
yasmim44 <yasmim128nr@gmail.com>
2026-03-04 18:20:00 -0300

    implementaçao do bootstrap

---
+++ app/Http/Sobre.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Sobre extends Controller
+{
+    function about(){
+        return view ('pagina-about');
+    }
+
+    
+}
