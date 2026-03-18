89e87e7f7f6af779e8bc9cf167559cf8a7dbb542
yasmim44 <yasmim128nr@gmail.com>
2026-03-18 18:21:15 -0300

    18032025

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -9,8 +9,28 @@ class Principal extends Controller
     function home(){
         return view ('pagina-home');
     }
-
-    
+    function contato(string $nome){
+        echo "o nome do contato é: $nome";
+        $contato = [
+            "nome" => $nome
+        ];
+        return view('contato', $contato);
+    }
+    function contact(string $nome, string $sobrenome){
+        echo "o nome completo é: $nome $sobrenome";
+    }
+    function assunto(string $nome, string $sobrenome, string $assunto){
+        echo "<h1>Contato: $nome $sobrenome</h1>";
+        echo "<marquee>$assunto</marquee>";
+    }
+    function mensagem(string $nome, string $sobrenome, string $assunto, string $email, string $telefone = 'Telefone nao foi informado'){
+        echo "<h1>Contato: $nome $sobrenome</h1>";
+        echo "<marquee>$assunto</marquee>";
+        echo "E-mail: $email <br>";
+        echo "Telefone: $telefone";
+    }
 }
+
+
 
 
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,18 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'home'])->name('pagina-inicial');
-Route::get('/sobre', [App\Http\Controllers\Sobre::class, 'about'])->name('pagina-sobre');
+
+Route::prefix('publico')->group(function(){
+    Route::get('/sobre', [App\Http\Controllers\Sobre::class, 'about'])->name('pagina-sobre');
+    Route::get('/contato/{nome}', [App\Http\Controllers\Principal::class, 'contact'])->name('contato');
+    Route::get('/contato/{nome}/{sobrenome}', [App\Http\Controllers\Principal::class, 'complet'])->name('contatoNomeCompleto');
+    Route::get('/contato/{nome}/{sobrenome}/{assunto}', [App\Http\Controllers\Principal::class, 'assunto'])->name('assunto');
+    Route::get('/contato/{nome}/{sobrenome}/{assunto}/{email}/{telefone?}', [App\Http\Controllers\Principal::class, 'mensagem'])->name('mensagem');
+    
+});
 
 
-
-
+Route::fallback(function(){
+    echo "A rota acessada nao existe.";
+    echo "<a href='". route('pagina-inicial'). "'>Voltar</a>";
+});
